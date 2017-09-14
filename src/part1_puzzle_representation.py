94a9d2d25422e4dead7157edf8504676f94cea44
elyfialkoff <mouseen92@gmail.com>
2017-09-14 16:54:11 -0400

    Grid Complete

--- src/part1_puzzle_representation.py
+++ src/part1_puzzle_representation.py
@@ -9,15 +9,22 @@ import random
 
 # Ask user for puzzle size
 n = input('Enter the size of the puzzle grid (5, 7, 9, 11): ')
+#n = 5
 n = int(n)
 
-# Generates a 2D matrix
-puz = [[random.randint(1,n-1) for x in range(n)] for y in range(n)]
-puz[n - 1][n - 1] = 0
+## ----- Generates a 2D Valid matrix ----- ##
+puz = [[0 for x in range(n)] for y in range(n)]
+for x in range(0, n):
+    for y in range(0, n):
+        for k in range(0, int((n - 1)/2) + 1):
+            if(x == k or x == n - (k + 1) or y == k or y == n - (k +1)):
+                puz[x][y] = random.randint(1, n - (k + 1))
+puz[n-1][n-1] = 0
+## ----- End Valid Matrix ----- ##
 
 # Print the randomly generated puzzle
 for x in range(0,n):
     for y in range(0,n):
         print(puz[x][y], end = '')
     print()
-        
+     
