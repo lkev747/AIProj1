40253d34a3e7e53e2ed40aca5832fd606ab4d38a
lkev747 <lkev747@gmail.com>
2017-09-14 16:17:11 -0400

    Update

--- src/part1_puzzle_representation.py
+++ src/part1_puzzle_representation.py
@@ -8,13 +8,12 @@ Created on Sep 13, 2017
 import random
 
 # Ask user for puzzle size
-# n = input('Enter the size of the puzzle grid (5, 7, 9, 11): ')
-n = 5
-# n = int(n)
+n = input('Enter the size of the puzzle grid (5, 7, 9, 11): ')
+n = int(n)
 
 # Generates a 2D matrix
 puz = [[random.randint(1,n-1) for x in range(n)] for y in range(n)]
-puz[n][n] = 0
+puz[n - 1][n - 1] = 0
 
 # Print the randomly generated puzzle
 for x in range(0,n):
@@ -22,4 +21,3 @@ for x in range(0,n):
         print(puz[x][y], end = '')
     print()
         
-print('hello world')
