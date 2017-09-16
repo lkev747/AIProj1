6014c9ad5c0753735883d86fd926d1d8e4fc944a
elyfialkoff <mouseen92@gmail.com>
2017-09-16 15:53:21 -0400

    Task 2 Completed

--- src/part2_puzzle_evaluation.py
+++ src/part2_puzzle_evaluation.py
@@ -51,6 +51,7 @@ for x in range(0,n):
         nodes[x][y]['value'] = puz[x][y]
         nodes[x][y]['xcoord'] = x
         nodes[x][y]['ycoord'] = y
+        nodes[x][y]['level'] = 0
 ## ----- End Create Nodes ----- ##
 
 
@@ -72,34 +73,38 @@ while len(q) != 0:
     
     
     if (w['xcoord'] + w['value'] <= n-1) and (visited[w['xcoord'] + w['value']][w['ycoord']] == 0): 
-        visited[w['xcoord'] + w['value']][w['ycoord']] = 1 # Mark new node as visited
+        visited[w['xcoord'] + w['value']][w['ycoord']] = w['level'] + 1 # Mark new node as visited
         q.append(nodes[ w['xcoord'] + w['value']][w['ycoord']]) # Add child node to queue
         nodes[w['xcoord'] + w['value']][w['ycoord']]['parent'] = w
+        nodes[w['xcoord'] + w['value']][w['ycoord']]['level'] = w['level'] + 1
         has_children = True
         
     if (w['xcoord'] - w['value'] >= 0) and (visited[w['xcoord'] - w['value']][w['ycoord']] == 0): 
-        visited[ w['xcoord'] - w['value'] ][ w['ycoord'] ] = 1 # Mark new node as visited
+        visited[ w['xcoord'] - w['value'] ][ w['ycoord'] ] = w['level'] + 1 # Mark new node as visited
         q.append(nodes[w['xcoord'] - w['value']][w['ycoord']]) # Add child node to queue
         nodes[w['xcoord'] - w['value']][w['ycoord']]['parent'] = w
+        nodes[w['xcoord'] - w['value']][w['ycoord']]['level'] = w['level'] + 1
         has_children = True
         
     if (w['ycoord'] + w['value'] <= n-1) and (visited[w['xcoord']][w['ycoord'] + w['value']] == 0): 
-        visited[w['xcoord']][w['ycoord'] + w['value']] = 1 # Mark new node as visited
+        visited[w['xcoord']][w['ycoord'] + w['value']] = w['level'] + 1 # Mark new node as visited
         q.append(nodes[w['xcoord']][w['ycoord'] + w['value']]) # Add child node to queue
         nodes[w['xcoord']][w['ycoord'] + w['value']]['parent'] = w
+        nodes[w['xcoord']][w['ycoord'] + w['value']]['level'] = w['level'] + 1
         has_children = True
         
     if (w['ycoord'] - w['value'] >= 0) and (visited[w['xcoord']][w['ycoord'] - w['value']] == 0): 
-        visited[ w['xcoord']][w['ycoord'] - w['value']] = 1 # Mark new node as visited
+        visited[ w['xcoord']][w['ycoord'] - w['value']] = w['level'] + 1 # Mark new node as visited
         q.append(nodes[w['xcoord']][ w['ycoord'] - w['value']]) # Add child node to queue
         nodes[w['xcoord']][w['ycoord'] - w['value']]['parent'] = w
+        nodes[w['xcoord']][w['ycoord'] - w['value']]['level'] = w['level'] + 1
         has_children = True
 #    print()
 ## ----- End Breadth-First Search ----- ##
 
 
 ## ----- Print Successful Path ----- #
-if len(nodes[n-1][n-1]) == 4:
+if len(nodes[n-1][n-1]) == 5: #########
     print('Successful Path: ')
 
     goalpath = []
@@ -118,16 +123,31 @@ if len(nodes[n-1][n-1]) == 4:
         print('(', goalpath[i]['xcoord'], ', ', goalpath[i]['ycoord'], ')', end = '')
     print()
     print('Value Function: ', len(goalpath) - 1)
+    print()
+    
+    visited[0][0] = 0
+    for x in range(0, n):
+        for y in range(0, n):
+            if visited[x][y] == 0 and (x != 0 or y != 0):
+                visited[x][y] = 'X'
+            print(visited[x][y], end = '')
+        print()    
+
 else:
     print("No successful path!")
+    visited[0][0] = 0
     k = 0
     for x in range(0, n):
         for y in range(0, n):
-            print(visited[x][y], end = '')
-            if visited[x][y] == 0:
+            if visited[x][y] == 0 and (x != 0 or y != 0):
+                visited[x][y] = 'X'
                 k = k + 1
-        print()   
+            print(visited[x][y], end = '')
+        print()
     print('Value Function: -', k)
+    print()
+    
+  
 ## ----- End Print Successful Path ----- ##
 
 
