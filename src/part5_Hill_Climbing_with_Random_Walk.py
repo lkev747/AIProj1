ec8f88510fb0420ba826a82ed8c13cb926479488
elyfialkoff <mouseen92@gmail.com>
2017-09-20 14:00:25 -0400

    Big steps Forward, part 6 done

---
+++ src/part5_Hill_Climbing_with_Random_Walk.py
@@ -0,0 +1,98 @@
+'''
+Created on Sep 20, 2017
+
+@author: Kevin and Ely
+'''
+## ----- Import Statements ----- ##
+import random
+from part1_puzzle_representation import generate_puzzle
+from part1_puzzle_representation import print_matrix
+from part2_puzzle_evaluation import print_path
+from part2_puzzle_evaluation import BFS
+## ----- End Import Statements ----- ##
+
+## ----- Hill-Climbing w Random Walk ----- ##
+def hill_climb_random_walk(nodes, n, iteration, probability): 
+    for i in range(0, iteration):
+        visited, oldk = BFS(nodes, n)
+        tempnode = 0
+        
+        ## ----- Choose Random Cell ----- ##
+        a = True
+        randx = 0
+        randy = 0
+        while a:
+            randx = random.randint(0, n - 1)
+            randy = random.randint(0, n - 1)
+            if not (randx == n - 1 and randy == n - 1):
+                tempnode = nodes[randx][randy]['value']
+                a = False
+        ## ----- End Choose Random Cell ----- ##
+        
+        
+        ## ----- Choose Random Value ----- ##
+        temp = 0
+        b = True
+        while b:
+            for k in range(0, int((n - 1)/2) + 1):
+                if(randx == k or randx == n - (k + 1) or randy == k or randy == n - (k + 1)): 
+                    temp = random.randint(1, n - (k + 1))
+                    if temp != nodes[randx][randy]['value']:
+                        nodes[randx][randy]['value'] = temp
+                        b = False
+                        break
+        ## ----- End Choose Random Value ----- ##
+        
+        
+        ## ----- Print Details ----- ##
+        print()
+        print('Next Iteration: (', randx, ',', randy, ')',  end = '')
+        print(' | Old Value:', tempnode, '| New Value: ', temp)
+        ## ----- End Print Details ----- ##
+        
+        
+        ## ----- Print New Matrix ----- ##
+        print('New Matrix: ')
+        for x in range(0, n):
+            for y in range(0, n):
+                print(nodes[x][y]['value'], end = '')
+            print()
+        ## ----- End Print New Matrix ----- ##
+        
+        ## ----- Accept/Reject Change ----- ##
+        newvisited, newk = BFS(nodes, n)
+        print('Old k value:', oldk, end = '')
+        print('. New k value:', newk, end = '')
+        if(newk < oldk):
+            if (random.randint(1, 100) <= probability):
+                print('. Change Accepted with Random Walk')
+            else:
+                nodes[randx][randy]['value'] = tempnode # Apply Reversion
+                print('. Change Not Accepted')
+        else:
+            print('. Change Accepted')
+        ## ----- End Accept/Reject Change ----- ##
+        
+        
+    newvisited, newk = BFS(nodes, n)        
+    return nodes, newvisited, newk
+## ----- End Hill-Climbing w Random Walk ----- ##
+
+
+## ----- Unit Test ----- ##
+number_of_iterations = 50
+probability_of_accepting_random_walk = 10   # percentage
+
+nodes, n = generate_puzzle()
+print_matrix(nodes, n)
+nodes, newvisited, newk = hill_climb_random_walk(nodes, n, number_of_iterations, probability_of_accepting_random_walk)
+print_matrix(nodes, n)
+print_path(nodes, n, newvisited)
+## ----- End Unit Test ----- ##
+
+
+
+
+
+
+
---
+++ src/part6_Simulated_Annealing.py
@@ -0,0 +1,89 @@
+'''
+Created on Sep 20, 2017
+
+@author: Kevin and Ely
+'''
+## ----- Import Statements ----- ##
+import random
+import math
+from part1_puzzle_representation import generate_puzzle
+from part1_puzzle_representation import print_matrix
+from part2_puzzle_evaluation import print_path
+from part2_puzzle_evaluation import BFS
+## ----- End Import Statements ----- ##
+
+## ----- Hill-Climbing w Simulated Annealing ----- ##
+def hill_climb_simulated_annealing(nodes, n, iteration, temperature, decay_constant): 
+    for i in range(0, iteration):
+        _, oldk = BFS(nodes, n)
+        tempnode = 0
+        
+        ## ----- Choose Random Cell ----- ##
+        a = True
+        randx = 0
+        randy = 0
+        while a:
+            randx = random.randint(0, n - 1)
+            randy = random.randint(0, n - 1)
+            if not (randx == n - 1 and randy == n - 1):
+                tempnode = nodes[randx][randy]['value']
+                a = False
+        ## ----- End Choose Random Cell ----- ##
+        
+        ## ----- Choose Random Value ----- ##
+        temp = 0
+        b = True
+        while b:
+            for k in range(0, int((n - 1)/2) + 1):
+                if(randx == k or randx == n - (k + 1) or randy == k or randy == n - (k + 1)): 
+                    temp = random.randint(1, n - (k + 1))
+                    if temp != nodes[randx][randy]['value']:
+                        nodes[randx][randy]['value'] = temp
+                        b = False
+                        break
+        ## ----- End Choose Random Value ----- ##
+        
+        ## ----- Print Details ----- ##
+        print()
+        print('Next Iteration: (', randx, ',', randy, ')',  end = '')
+        print(' | Old Value:', tempnode, '| New Value: ', temp)
+        ## ----- End Print Details ----- ##  
+        
+        ## ----- Print New Matrix ----- ##
+        print('New Matrix: ')
+        for x in range(0, n):
+            for y in range(0, n):
+                print(nodes[x][y]['value'], end = '')
+            print()
+        ## ----- End Print New Matrix ----- ##
+        
+        ## ----- Accept/Reject Change ----- ##
+        _, newk = BFS(nodes, n)
+        print('Old k value:', oldk, end = '')
+        print('. New k value:', newk, end = '')
+        if(newk < oldk):
+            if (random.random() <= math.exp((newk - oldk) / temperature)):
+                print('. Change Accepted with Simulated Annealing', '| ', temperature)
+            else:
+                nodes[randx][randy]['value'] = tempnode # Apply Reversion    
+                print('. Change Not Accepted')
+        else:
+            print('. Change Accepted')
+        ## ----- End Accept/Reject Change ----- ##
+        temperature *= decay_constant
+        
+        
+    newvisited, newk = BFS(nodes, n)        
+    return nodes, newvisited, newk
+## ----- End Hill-Climbing w Simulated Annealing ----- ##
+
+## ----- Unit Test ----- ##
+number_of_iterations = 200
+initial_temp = 100
+initial_d = .7
+nodes, n = generate_puzzle()
+print_matrix(nodes, n)
+nodes, newvisited, newk = hill_climb_simulated_annealing(nodes, n, number_of_iterations, initial_temp, initial_d)
+print_matrix(nodes, n)
+print_path(nodes, n, newvisited)
+## ----- End Unit Test ----- ##
