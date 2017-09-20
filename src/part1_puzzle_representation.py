a7eba4f81f2d5d35cb2a8eea5c4cb63d5e7f4009
lkev747 <lkev747@gmail.com>
2017-09-19 20:56:39 -0400

    Modularity Update 3

--- src/part1_puzzle_representation.py
+++ src/part1_puzzle_representation.py
@@ -25,6 +25,39 @@ def generate_puzzle():
     puz[n-1][n-1] = 0
     ## ----- End Valid Matrix ----- ##
     
+    ## ----- Unit Tests ----- ##
+    '''
+    # 19 Moves - Initial Example
+    puz = [[3, 2, 1, 4, 1],
+           [3, 2, 1, 3, 3],
+           [3, 3, 2, 1, 4],
+           [3, 1, 2, 3, 3],
+           [1, 4, 4, 3, 0]]
+    
+    n = 5
+    '''
+    '''
+    # Pass - Value Function 5 Example
+    puz = [[2, 2, 2, 4, 3],
+           [2, 2, 3, 3, 3],
+           [3, 3, 2, 3, 3],
+           [4, 3, 2, 2, 2],
+           [1, 2, 1, 4, 0],]
+    
+    n = 5
+    '''
+    '''
+    # Fail - Value Function -3 Example
+    puz = [[3, 3, 2, 4, 3],
+           [2, 2, 2, 1, 1],
+           [4, 3, 1, 3, 4],
+           [2, 3, 1, 1, 3],
+           [1, 1, 3, 2, 0]]
+    
+    n = 5
+    '''
+    ## ----- End Unit Tests ----- ##
+    
 
     ## ----- Create Nodes ----- ##
     nodes = [[{} for x in range(n)] for y in range(n)] # Store each node as a dictionary
