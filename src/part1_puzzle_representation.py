470092a81518fef1971d19ad78a0176ae9d05e6f
lkev747 <lkev747@gmail.com>
2017-09-19 20:40:14 -0400

    Modularity Update

--- src/part1_puzzle_representation.py
+++ src/part1_puzzle_representation.py
@@ -7,25 +7,44 @@ Created on Sep 13, 2017
 # Import statements
 import random
 
-## ----- Input Puzzle Size ----- ##
-n = input('Enter the size of the puzzle grid (5, 7, 9, 11): ')  # need to validate
-n = int(n)
-## ----- End Input Puzzle Size ----- ##
 
-## ----- Valid Matrix ----- ##
-puz = [[0 for x in range(n)] for y in range(n)]
-for x in range(0, n):
-    for y in range(0, n):
-        for k in range(0, int((n - 1)/2) + 1):
-            if(x == k or x == n - (k + 1) or y == k or y == n - (k +1)):
-                puz[x][y] = random.randint(1, n - (k + 1))
-puz[n-1][n-1] = 0
-## ----- End Valid Matrix ----- ##
+## ----- Generate Random Puzzle ----- ##
+def generate_puzzle():
+    ## ----- Input Puzzle Size ----- ##
+    n = input('Enter the size of the puzzle grid (5, 7, 9, 11): ')  # need to validate
+    n = int(n)
+    ## ----- End Input Puzzle Size ----- ##
+    
+    ## ----- Valid Matrix ----- ##
+    puz = [[0 for x in range(n)] for y in range(n)]
+    for x in range(0, n):
+        for y in range(0, n):
+            for k in range(0, int((n - 1)/2) + 1):
+                if(x == k or x == n - (k + 1) or y == k or y == n - (k +1)):
+                    puz[x][y] = random.randint(1, n - (k + 1))
+    puz[n-1][n-1] = 0
+    ## ----- End Valid Matrix ----- ##
+    
+
+    ## ----- Create Nodes ----- ##
+    nodes = [[{} for x in range(n)] for y in range(n)] # Store each node as a dictionary
+    for x in range(0,n):
+        for y in range(0,n):
+            nodes[x][y]['value'] = puz[x][y]
+            nodes[x][y]['xcoord'] = x
+            nodes[x][y]['ycoord'] = y
+            nodes[x][y]['level'] = 0
+    ## ----- End Create Nodes ----- ##
+
+    return nodes, n
+## ----- End Generate Random Puzzle ----- ##
+
 
 ## ----- Print Matrix ----- ##
-for x in range(0,n):
-    for y in range(0,n):
-        print(puz[x][y], end = '')
-    print()
+def print_matrix(nodes, n):
+    print('Randomly Generated Puzzle: ')
+    for x in range(0,n):
+        for y in range(0,n):
+            print(nodes[x][y]['value'], end = '')
+        print()
 ## ----- End Print Matrix ----- ##
-
--- src/part2_puzzle_evaluation.py
+++ src/part2_puzzle_evaluation.py
@@ -4,9 +4,10 @@ Created on Sep 14, 2017
 @author: Kevin, Ely
 '''
 
+
 ## ----- Import Statements ----- ##
-from part1_puzzle_representation import puz
-from part1_puzzle_representation import n
+from part1_puzzle_representation import generate_puzzle
+from part1_puzzle_representation import print_matrix
 ## ----- End Import Statements ----- ##
 
 
@@ -44,17 +45,6 @@ n = 5
 ## ----- End Unit Tests ----- ##
 
 
-## ----- Create Nodes ----- ##
-nodes = [[{} for x in range(n)] for y in range(n)] # Store each node as a dictionary
-for x in range(0,n):
-    for y in range(0,n):
-        nodes[x][y]['value'] = puz[x][y]
-        nodes[x][y]['xcoord'] = x
-        nodes[x][y]['ycoord'] = y
-        nodes[x][y]['level'] = 0
-## ----- End Create Nodes ----- ##
-
-
 ## ----- Breadth-First Search ----- ##
 def BFS(nodes, n):
     q = [] # Queue
@@ -63,12 +53,7 @@ def BFS(nodes, n):
     q.append(nodes[0][0]) # Enqueue the start point
     visited[0][0] = 1 # Mark start point as visited
     
-    while len(q) != 0:
-        
-    #    for i in range(0, len(q)):
-    #        print('(', q[i]['xcoord'], ', ', q[i]['ycoord'], ')', end = '')
-    #        print(', ', q[i]['value'], '; ', end = '')
-        
+    while len(q) != 0:        
         w = q.pop(0) # Dequeue from q (returns node dictionary)        
         
         if (w['xcoord'] + w['value'] <= n-1) and (visited[w['xcoord'] + w['value']][w['ycoord']] == 0): 
@@ -94,58 +79,61 @@ def BFS(nodes, n):
             q.append(nodes[w['xcoord']][ w['ycoord'] - w['value']]) # Add child node to queue
             nodes[w['xcoord']][w['ycoord'] - w['value']]['parent'] = w
             nodes[w['xcoord']][w['ycoord'] - w['value']]['level'] = w['level'] + 1
-    #    print()
-    
+
     return visited
 ## ----- End Breadth-First Search ----- ##
 
-visited = BFS(nodes, n)
 
 ## ----- Print Successful Path ----- #
-if len(nodes[n-1][n-1]) == 5: #########
-    print('Successful Path: ')
-
-    goalpath = []
-    goalpath.append(nodes[n-1][n-1])
-    par = nodes[n-1][n-1]['parent']
+def print_path(nodes, n, visited):
+    if len(nodes[n-1][n-1]) == 5: 
+        print('Successful Path: ')
     
-    while par['xcoord'] != 0 or par['ycoord'] != 0:
-        goalpath.append(par)
-        xc = par['xcoord']
-        yc = par['ycoord']
-        par = nodes[xc][yc]['parent']
-    
-    goalpath.append(nodes[0][0])
-    goalpath.reverse()
-    for i in range(0, len(goalpath)):
-        print('(', goalpath[i]['xcoord'], ', ', goalpath[i]['ycoord'], ')', end = '')
-    print()
-    print('Value Function: ', len(goalpath) - 1)
-    print('Visited Matrix: ')
-    
-    visited[0][0] = 0
-    for x in range(0, n):
-        for y in range(0, n):
-            if visited[x][y] == 0 and (x != 0 or y != 0):
-                visited[x][y] = 'X'
-            print(visited[x][y], end = '')
-        print()    
-
-## Fix K value for No successful path
-else:
-    print("No successful path!")
-    visited[0][0] = 0
-    k = 0
-    for x in range(0, n):
-        for y in range(0, n):
-            if visited[x][y] == 0 and (x != 0 or y != 0):
-                visited[x][y] = 'X'
-                k = k + 1
-            print(visited[x][y], end = '')
+        goalpath = []
+        goalpath.append(nodes[n-1][n-1])
+        par = nodes[n-1][n-1]['parent']
+        
+        while par['xcoord'] != 0 or par['ycoord'] != 0:
+            goalpath.append(par)
+            xc = par['xcoord']
+            yc = par['ycoord']
+            par = nodes[xc][yc]['parent']
+        
+        goalpath.append(nodes[0][0])
+        goalpath.reverse()
+        for i in range(0, len(goalpath)):
+            print('(', goalpath[i]['xcoord'], ', ', goalpath[i]['ycoord'], ')', end = '')
         print()
-    print('Value Function: ', -1 * k)
-    print()
+        print('Value Function: ', visited[n-1][n-1])
+        print('Visited Matrix: ')
+        
+        visited[0][0] = 0
+        for x in range(0, n):
+            for y in range(0, n):
+                if visited[x][y] == 0 and (x != 0 or y != 0):
+                    visited[x][y] = 'X'
+                print(visited[x][y], end = '')
+            print()    
     
-  
+        return visited[n-1][n-1]
+    else:
+        print("No successful path!")
+        visited[0][0] = 0
+        k = 0
+        for x in range(0, n):
+            for y in range(0, n):
+                if visited[x][y] == 0 and (x != 0 or y != 0):
+                    visited[x][y] = 'X'
+                    k = k + 1
+                print(visited[x][y], end = '')
+            print()
+        print('Value Function: ', -1 * k)
+        print()
+    
 ## ----- End Print Successful Path ----- ##
+
+nodes, n = generate_puzzle()
+print_matrix(nodes,n)
+visited = BFS(nodes, n)
+print_path(nodes, n, visited)
 
