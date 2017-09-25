6260f108ad0fa3c7f50d54d1a445a1e30c83b1b4
elyfialkoff <mouseen92@gmail.com>
2017-09-25 16:35:00 -0400

    working on 7

--- src/part7_Population_Based_Approach.py
+++ src/part7_Population_Based_Approach.py
@@ -25,7 +25,7 @@ def genetic_algorithm(puzzles, number_of_puzzles, size_of_puzzles): # puzzles is
         population.append(flattened_puzzle)
     ## ----- End Flatten Puzzle ----- ##
   
-  
+    '''
     ##### Print The Flattenened Matrices
     print('Flattened Matrices: ')
     for i in range(0, number_of_puzzles):
@@ -33,7 +33,8 @@ def genetic_algorithm(puzzles, number_of_puzzles, size_of_puzzles): # puzzles is
             print(population[i][j]['value'], ', ', end = '')
         print()
     ##### End Print
-        
+    '''
+         
     ## ----- Evaluate Fitness ----- ##
     k_values = [] # k values of the puzzles
     survivability = [] # percent chance of the survivability of the puzzle
@@ -83,22 +84,33 @@ def genetic_algorithm(puzzles, number_of_puzzles, size_of_puzzles): # puzzles is
             temp = random.random()
             for i in selection:
                 if temp < i:
+                    selected_pop.append(population[index])
+                    index += 1
+                    flag = False
                     break
-                index += 1
+                
+                
+            '''
             if(j > 0):
                 if (selected_pop[-1] == population[index]):
+                    print("I am the same as the previous")
+                    print(population[index])
                     flag = True
                 else:
                     selected_pop.append(population[index])
+                    print("I will be appended")
+                    print(population[index])
                     flag = False 
             else:
                 selected_pop.append(population[index])
+                print("I am the first")
+                print(population[index])
                 flag = False
-            
+            '''
         
     ## ----- End Selection Step ----- ##
     
-    
+    '''
     ##### Print The Flattenened Matrices
     print('Selected Matrices: ')
     for i in range(0, number_of_puzzles):
@@ -107,7 +119,7 @@ def genetic_algorithm(puzzles, number_of_puzzles, size_of_puzzles): # puzzles is
         print()
     print()
     ##### End Print
-    
+    '''
     
     ## ----- Crossover Step ----- ##
     if len(selected_pop) % 2 > 0: # in case of odd number of puzzles
@@ -118,13 +130,14 @@ def genetic_algorithm(puzzles, number_of_puzzles, size_of_puzzles): # puzzles is
         par1 = selected_pop.pop(0)
         par2 = selected_pop.pop(0)
         split_location = random.randint(1, len(population[0]) - 1)
-        print('Crossed Matrices at: ', split_location)
+    #   print('Crossed Matrices at: ', split_location)
         crossover_pop.append(par1[0:split_location] + par2[split_location:])
         split_location = random.randint(1, len(population[0]) - 1)
-        print('Crossed Matrices at: ', split_location)
+    #   print('Crossed Matrices at: ', split_location)
         crossover_pop.append(par2[0:split_location] + par1[split_location:])
     ## ----- End Crossover Step ----- ##
     
+    '''
     ## ----- Print The Flattenened Matrices ----- ##
     for i in range(0, number_of_puzzles):
         for j in range(0, len(crossover_pop[0])):
@@ -132,7 +145,8 @@ def genetic_algorithm(puzzles, number_of_puzzles, size_of_puzzles): # puzzles is
         print()
     print()
     ## ----- End Print the Flattened Matrices----- ##
-    
+    '''
+        
     ## ----- Reshape Puzzles ----- ##
     next_gen = [] # array of square matrix puzzles
     for i in range(0, number_of_puzzles):
@@ -141,7 +155,41 @@ def genetic_algorithm(puzzles, number_of_puzzles, size_of_puzzles): # puzzles is
             temp_matrix[math.floor(j / size_of_puzzles)][j % size_of_puzzles] = crossover_pop[i][j]
         next_gen.append(temp_matrix)
     ## ----- End Reshape Puzzles ----- ##
+    ## --- START
+    for i in range(0, number_of_puzzles): 
+        for w in range(0, size_of_puzzles):
+            tempnode = 0
+         ## ----- Choose Random Cell ----- ##
+            a = True
+            randx = 0
+            randy = 0
+            while a:
+                randx = random.randint(0, size_of_puzzles - 1)
+                randy = random.randint(0, size_of_puzzles - 1)
+                if not (randx == size_of_puzzles - 1 and randy == size_of_puzzles - 1):
+                    tempnode = next_gen[i][randx][randy]['value']
+                    a = False
+            ## ----- End Choose Random Cell ----- ##
+            
+            ## ----- Choose Random Value ----- ##
+            temp = 0
+            b = True
+            while b:
+                for k in range(0, int((size_of_puzzles - 1)/2) + 1):
+                    if(randx == k or randx == size_of_puzzles - (k + 1) or randy == k or randy == size_of_puzzles - (k + 1)): 
+                        temp = random.randint(1, size_of_puzzles - (k + 1))
+                        if temp != next_gen[i][randx][randy]['value']:
+                            next_gen[i][randx][randy]['value'] = temp
+                            b = False
+                            break
+            ## ----- End Choose Random Value ----- ##
     
+    
+    
+    
+    ## --- END
+
+    '''
     ## Mutation for all Puzzles
     for i in range(0, number_of_puzzles):
     ## Mutation Step for each Puzzle ##
@@ -159,7 +207,6 @@ def genetic_algorithm(puzzles, number_of_puzzles, size_of_puzzles): # puzzles is
                     mutate_x[m] = randx
                     mutate_y[m] = randy
                     pick_again = False
-                    break
         print(mutate_x, mutate_y)       
             ## Pick a random number
         mutate_value = [-1]*size_of_puzzles
@@ -171,7 +218,7 @@ def genetic_algorithm(puzzles, number_of_puzzles, size_of_puzzles): # puzzles is
                     if(mutate_x[p] == k or mutate_x[p] == size_of_puzzles - (k + 1) or mutate_y[p] == k or mutate_y[p] == size_of_puzzles - (k + 1)): 
                             temp_value = random.randint(1, size_of_puzzles - (k + 1))
                             if not (temp_value == next_gen[i][mutate_x[p]][mutate_y[p]]['value']): # meaning the random value is different from the previous value already in the cell
-                                #next_gen[mutate_x[p]][mutate_y[p]]['value'] = temp_value
+                                #next_gen[i][mutate_x[p]][mutate_y[p]]['value'] = temp_value
                                 mutate_value[p] = temp_value
                                 pick_again = False
                                 break
@@ -180,20 +227,31 @@ def genetic_algorithm(puzzles, number_of_puzzles, size_of_puzzles): # puzzles is
         for q in range(0, size_of_puzzles):
             next_gen[i][mutate_x[q]][mutate_y[q]]['value'] = mutate_value[q]
         
+        
+        print_matrix(next_gen[i], size_of_puzzles)
         ## print out next gen
-        '''
+    
+        
+    
         for w in range(0, number_of_puzzles):
             for x in range(0, size_of_puzzles):
                 for y in range(0, size_of_puzzles):
                     print(next_gen[w][x][y]['value'], ', ', end='')
                 print()
             print()
-        '''
-
+    
+        
+    
+    print("            After")
+    for i in range(0, number_of_puzzles):
+        print_matrix(next_gen[i], size_of_puzzles)
+    '''
     return next_gen 
     ## End Mutation Step for each Puzzle ##
-    
     ## End Mutation Step for all Puzzles ##
+       
+    ## Print here
+    
        
     ## ----- Mutation Step ----- ##
     '''
